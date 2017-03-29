0c78fa77c00cc3d1356c0fb67ca27283ddb1d2d3
Joel Nises <joel@nises.se>
2017-03-29 19:54:17 +0200

    fadeout

--- webglstarfield/main.py
+++ webglstarfield/main.py
@@ -9,7 +9,6 @@ class Main(webapp2.RequestHandler):
 	<head>
 		<title>WebGL Starfield</title>
 		<meta charset="utf-8">
-        <script type="text/javascript" src="starfield_min.js"></script>
         <style type="text/css">
           body
           {
@@ -21,12 +20,17 @@ class Main(webapp2.RequestHandler):
           {
           color: #dddddd;
           }
+        .hidden {
+        visibility: hidden;
+        opacity: 0;
+        transition: visibility 0s 2s, opacity 2s ease;
+        }
         </style>
     </head>
     <body>
       <div id = "starfield" style = "z-index: -1; position: fixed; top: 0px; left: 0px; width: 100%; height: 100%;">
       </div>
-      <div style = "z-index: 1;">
+      <div id="overlay" style = "z-index: 1;">
         <div style="width: 800px; height: 1000px; margin-left: auto; margin-right: auto; background-image: url('bg.png');">
           <div style="margin-left: 20px; margin-top: 10px;">
             <h2>My god it's full of stars!</h2>
@@ -34,20 +38,17 @@ class Main(webapp2.RequestHandler):
               The background of this page is - as long as you have the correct browser - full of stars.<br/>
               They are all rendered using webgl.
             </p>
-<p>
-The correct browser would in this case be something like <a href="http://firefox.com">Firefox</a> or <a href="http://google.com/chrome">Chrome</a>.<br/>
-If you are from the future even Internet Explorer might work.
-</p>
-            <p>
-              If things doesn't work, try refreshing the page.
-            </p>
           </div>
         </div>
       </div>
 
+      <script type="text/javascript" src="starfield_min.js"></script>
       <script type="text/javascript">
         var starfield = new Starfield(document.getElementById("starfield"));
         starfield.render();
+        setTimeout(function() {
+            document.getElementById("overlay").className += "hidden";
+        }, 5000);
       </script>
     </body>
 </html>
